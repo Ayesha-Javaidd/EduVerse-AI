d6cdd282d907850bae2b6126a54d9cd62b707880
Ayesha Javaid <ayeshajavaid5005@gmail.com>
2026-05-16 16:39:13 +0500

    Changed env for render deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://eduverse-ai-backend.up.railway.app',
+  apiBaseUrl: 'https://eduverse-ai-main-backend.onrender.com',
   appName: 'EduVerse',
   supportEmail: 'support@eduverse.com',
   maxSubscriptionPlans: 3,
